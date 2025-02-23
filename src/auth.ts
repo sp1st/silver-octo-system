8d927077ff8bf86f812f5797ac2f8b7f59f5bb2c
28oso <28osoblanco2020@gmail.com>
2025-02-23 13:18:26 +0900

    feat: add database & Login/Logout

---
+++ src/auth.ts
@@ -0,0 +1,11 @@
+//src/auth.ts
+import NextAuth from "next-auth"
+// import GitHub from "next-auth/providers/github"
+import Google from "next-auth/providers/google"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { prisma } from "./lib/prisma"
+ 
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [Google],
+})
