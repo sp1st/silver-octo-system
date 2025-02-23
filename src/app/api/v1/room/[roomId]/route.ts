a2a81bfda746bb6cd5db6bbc8da163188c93d384
28oso <28osoblanco2020@gmail.com>
2025-02-24 00:33:12 +0900

    feat: GET list of rooms & cleanings

---
+++ src/app/api/v1/room/[roomId]/route.ts
@@ -0,0 +1,16 @@
+import { prisma } from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+const GET = async (req: NextRequest, { params }: { params: {roomId: string} }) => {
+    const {roomId} = await params
+
+    const cleanings = await prisma.cleaning.findMany({where: {roomId: roomId}});
+
+    if (!cleanings) {
+        return NextResponse.json(cleanings, { status: 404 });
+    }
+
+    return NextResponse.json(cleanings, { status: 200 });
+}
+
+export { GET }
