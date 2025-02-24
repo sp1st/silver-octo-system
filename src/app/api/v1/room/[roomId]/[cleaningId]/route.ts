387482cba4cd76c1877cd726c992f120e72639a3
28oso <28osoblanco2020@gmail.com>
2025-02-24 19:35:54 +0900

    fix: user_cleaning_route

--- src/app/api/v1/room/[roomId]/[cleaningId]/route.ts
+++ src/app/api/v1/room/[roomId]/[cleaningId]/route.ts
@@ -1,16 +1,21 @@
 import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
-const GET = async (req: NextRequest, { params }: { params: { roomId: string, cleaningId: string } }) => {
-    const { roomId, cleaningId } = params;
+const GET = async (
+  req: NextRequest,
+  context: { params: { roomId: string; cleaningId: string } }
+) => {
+  const { roomId, cleaningId } = context.params;
 
-    const cleaning = await prisma.cleaning.findMany({ where: { roomId: roomId, cleaningId: Number(cleaningId) } });
+  const cleaning = await prisma.cleaning.findMany({
+    where: { roomId, cleaningId: Number(cleaningId) },
+  });
 
-    if (!cleaning) {
-        return NextResponse.json(cleaning, { status: 404 });
-    }
+  if (!cleaning || cleaning.length === 0) {
+    return NextResponse.json({ error: "Not found" }, { status: 404 });
+  }
 
-    return NextResponse.json(cleaning, { status: 200 });
-}
+  return NextResponse.json(cleaning, { status: 200 });
+};
 
-export { GET }
+export { GET };
