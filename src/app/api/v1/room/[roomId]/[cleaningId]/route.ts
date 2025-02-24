eb6b3e15f5ddd64596017807c3e2c4004a2ce4fa
28oso <28osoblanco2020@gmail.com>
2025-02-24 19:28:40 +0900

    fix

--- src/app/api/v1/room/[roomId]/[cleaningId]/route.ts
+++ src/app/api/v1/room/[roomId]/[cleaningId]/route.ts
@@ -1,10 +1,10 @@
 import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
-const GET = async (req: NextRequest, { params }: { params: {cleaningId: string} }) => {
-    const {cleaningId} = params
+const GET = async (req: NextRequest, { params }: { params: { roomId: string, cleaningId: string } }) => {
+    const { roomId, cleaningId } = params;
 
-    const cleaning = await prisma.cleaning.findMany({where: {cleaningId: Number(cleaningId)}});
+    const cleaning = await prisma.cleaning.findMany({ where: { roomId: roomId, cleaningId: Number(cleaningId) } });
 
     if (!cleaning) {
         return NextResponse.json(cleaning, { status: 404 });
--- src/app/api/v1/room/[roomId]/route.ts
+++ src/app/api/v1/room/[roomId]/route.ts
@@ -2,7 +2,7 @@ import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 const GET = async (req: NextRequest, { params }: { params: {roomId: string} }) => {
-    const {roomId} = await params
+    const {roomId} = params
 
     const cleanings = await prisma.cleaning.findMany({where: {roomId: roomId}});
 
