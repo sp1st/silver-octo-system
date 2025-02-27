6df650799cc9ab4b3c80d4f5de1d3cbf033ac592
esusaki <susaki2002@gmail.com>
2025-02-28 04:58:49 +0900

    ui:updated loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,8 +2,8 @@ export default function Loading(){
     return (
         <div className="m-2 text-[30px] text-center">
             Loading...
-            <div className="bg-gray-200 w-[300px] text-black mx-auto text-[20px] h-[200px] flex items-center justify-center">
-                勇者が走っているGIF
+            <div className="mx-auto flex items-center justify-center">
+                <img src="/riceball_coffee.gif" className="m-2"/>
             </div>
         </div>
     )
