1b71a332a301a1ab4018716463dba6ff286885b2
28oso <28osoblanco2020@gmail.com>
2025-06-15 18:08:47 +0900

    fix: prisma-singleton

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,22 @@
 import { PrismaClient } from "@prisma/client"
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
-export const prisma = globalForPrisma.prisma || new PrismaClient()
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
+// const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+// export const prisma = globalForPrisma.prisma || new PrismaClient()
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
+
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+declare global {
+  // eslint-disable-next-line no-var
+  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
+}
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+export { prisma }; // prisma を名前付きエクスポート
+
+if (process.env.NODE_ENV !== 'production') {
+  globalThis.prismaGlobal = prisma;
+}
